8183ed6816eb52b983e06fc1b1a69d36623ad21f
geovan897 <gg256@njit.edu>
2016-03-16 01:07:43 -0400

    can view a list of teams but need to format the table

--- welcome.php
+++ welcome.php
@@ -1,7 +1,7 @@
 <?php
+session_start(); //oirginal portion before changes
 
 
-session_start(); // original portion before changes 
 
 echo 'Welcome '.$_SESSION['username']; // this is the original portion before changes
 
@@ -9,7 +9,15 @@ $user = $_SESSION['username'];
 
 echo '<a href ="logout.php" target ="_blank"/><b> Logout</b></a>';
 
+ if ( isset( $_SESSION['username'] ) ){
+  $x =  "<p class='welcome'>Welcome, <a class='removealine' href='account.php' title=''>".$_SESSION['username']."</a></p>";
+  
+  
+  require_once('clientDB.php.inc');
+  $login = new clientDB("connect.ini");
+  $response = $login->teams();
 
+ }
 ?>
 
 
